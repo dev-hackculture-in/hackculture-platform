9d4b7bb0a514298654132a42883e113b76d58ebe
Dhanush K <dhanushkgowda2226@gmail.com>
2025-07-09 21:56:16 +0530

    add newly made FeaturedHacks component to home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,7 @@
 import Hero from "../components/hero";
 import HeroBg from "../components/svgs/hero-bg";
 import Logos from "../components/logos";
+import FeaturedHacks from "../components/featured-hacks";
 import Stub from "../components/stub";
 import Story from "../components/outcomes";
 import Banner from "../components/banner";
@@ -17,6 +18,7 @@ const Home = () => {
                 <HeroBg className="absolute pointer-events-none" />
             </div>
             <Logos />
+            <FeaturedHacks />
             <Stub />
             <Story />
             <Banner />
